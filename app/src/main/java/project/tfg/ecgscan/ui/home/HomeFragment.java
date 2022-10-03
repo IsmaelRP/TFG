2aabcad6fa3176fad4cce56bedb8a59bd390d7e8
Ismael <ismael98r@hotmail.com>
2022-10-03 20:47:37 +0200

    V.3
    Home page (basic and plain design) implemented with functionalities: [Take photo, Upload photo] and convert to Bitmap

--- app/src/main/java/project/tfg/ecgscan/ui/home/HomeFragment.java
+++ app/src/main/java/project/tfg/ecgscan/ui/home/HomeFragment.java
@@ -1,29 +1,32 @@
 package project.tfg.ecgscan.ui.home;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 
-import java.util.Objects;
+import java.io.IOException;
 
 import project.tfg.ecgscan.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
-    private NavController navController;
     private FragmentHomeBinding b;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setHasOptionsMenu(true);
     }
 
 
@@ -33,16 +36,55 @@ public class HomeFragment extends Fragment {
         return b.getRoot();
     }
 
+
+    private void saveImage(Bitmap image) {
+        b.imageView.setImageBitmap(image);
+    }
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        setupViews(view);
+        setupViews();
     }
 
-    private void setupViews(View view) {
-        navController = Objects.requireNonNull(Navigation.findNavController(view));
-        //b.btnLogin.setOnClickListener(v -> navController.navigate(StartFragmentDirections.desStartToLogin()));
-
+    private void setupViews() {
+        b.btnScan.setOnClickListener(v -> dispatchTakePictureIntent(MediaStore.ACTION_IMAGE_CAPTURE));
+        b.btnUpload.setOnClickListener(v -> dispatchSelectPictureIntent());
     }
+
+    private void dispatchSelectPictureIntent() {
+        Intent selectPictureIntent = new Intent();
+        selectPictureIntent.setType("image/*");
+        selectPictureIntent.setAction(Intent.ACTION_GET_CONTENT);
+
+        if (selectPictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            imageCallback.launch(selectPictureIntent);
+        }
+    }
+
+
+    private void dispatchTakePictureIntent(String action_code) {
+        Intent takePictureIntent = new Intent(action_code);
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            imageCallback.launch(takePictureIntent);
+        }
+    }
+
+    private ActivityResultLauncher<Intent> imageCallback = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    if (result.getData().getExtras() != null){
+                        saveImage((Bitmap) result.getData().getExtras().get("data"));
+                    }else{
+                        try {
+                            saveImage(MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), result.getData().getData()));
+                        } catch (IOException e) {
+                            System.out.println("Error handling media files");
+                        }
+                    }
+
+                }
+            });
 
 }
--- app/src/main/java/project/tfg/ecgscan/ui/login/LoginFragment.java
+++ app/src/main/java/project/tfg/ecgscan/ui/login/LoginFragment.java
@@ -30,7 +30,6 @@ public class LoginFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setHasOptionsMenu(true);
     }
 
 
